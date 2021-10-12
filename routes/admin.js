a1620f715031282cea0f814426771f73a3a7becc
Oswaldo <>
2021-10-12 18:23:50 -0500

    *finished* functioning and fully parameterized queries

--- routes/admin.js
+++ routes/admin.js
@@ -2,53 +2,31 @@ const express = require("express");
 const admin = express.Router();
 const db = require("../config/database")
 
-pokemon.post("/", async (req, res, next) => {
-	const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body
-
-	if (pok_name && pok_height && pok_weight && pok_base_experience) {
-		
-		 const query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
-		query+= `VALUES ('${pok_name}',${pok_height}, ${pok_wight}, ${pok_base_experience});`
-		
-
-		const rows = await db.query(query)
-
-		if (rows.affectedRows != 0) {
-			return res.status(201).json({code: 201, message : "Pokemon insertado correctamente"})
-		}
-
-		return res.status(500).json({code:500, message:"Ocurrio un error"})
-	}
-	return res.status(500).json({code:500, message:"Campos incompletos"})
-
-})
-
-pokemon.delete("/:id([0-9]{1,3})", async (req,res,next) => {
-	const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id};`
+admin.delete("/:id([0-9]{1,3})", async (req,res,next) => {
+	const query = `DELETE FROM user WHERE user_id=${req.params.id};`
 
 	const rows = await db.query(query)
 
 	if (rows.affectedRows == 1) {
-		return res.status(200).json({code: 201, message : "Pokemon borrado correctamente"})
+		return res.status(200).json({code: 201, message : "Usuario borrado correctamente"})
 	}
-	return res.status(404).json({code: 404, message : "Pokemon no encontrado"})
+	return res.status(404).json({code: 404, message : "Usuario no encontrado"})
 })
 
-pokemon.put("/:id([0-9]{1,3})", async (req,res,next) => {
-	const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
-	
+admin.put("/:id([0-9]{1,3})", async (req,res,next) => {
+	const { user_name, user_mail, user_password, user_lastname, user_phone, user_address } = req.body;
 	
 
-	if (pok_name && pok_height && pok_weight && pok_base_experience) {
+	if (user_name && user_mail && user_password && user_lastname && user_phone && user_address) {
 
-		let query = `UPDATE pokemon SET pok_name='${pok_name}', pok_height=${pok_height},`
-		query += `pok_weight=${pok_weight}, pok_base_experience=$(pok_base_experience} WHERE pok_id=${req.params.id};`
+		let query = `UPDATE user SET user_name=?, user_mail=?,`
+		query += `user_password=?, user_lastname=?, user_phone=?, user_address=? WHERE user_id=${req.params.id};`
 		
 
-		const rows = await db.query(query)
+		const rows = await db.query(query,[user_name,user_mail,user_password,user_lastname,user_phone,user_address])
 
 		if (rows.affectedRows != 0) {
-			return res.status(200).json({code: 200, message : "Pokemon actualizado correctamente"})
+			return res.status(200).json({code: 200, message : "Usuario actualizado correctamente"})
 		}
 
 		return res.status(500).json({code:500, message:"Ocurrio un error"})
@@ -57,17 +35,18 @@ pokemon.put("/:id([0-9]{1,3})", async (req,res,next) => {
 	return res.status(500).json({code:500, message:"Campos incompletos"})
 })
 
-pokemon.patch("/:id([0-9]{1,3})", async (req,res,next) => {	
+admin.patch("/:id([0-9]{1,3})", async (req,res,next) => {	
+	const field = Object.keys(req.body)[0]
+	const value = req.body[field]
 
-	if (req.body.pok_name) {
+	if (field && value) {
 
-		let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}' WHERE pok_id=${req.params.id};`
+		let query = `UPDATE user SET ${field}=? WHERE user_id=${req.params.id};`
 			
-
-		const rows = await db.query(query)
+		const rows = await db.query(query,[value])
 
 		if (rows.affectedRows != 0) {
-			return res.status(200).json({code: 200, message : "Pokemon actualizado correctamente"})
+			return res.status(200).json({code: 200, message : "Campo actualizado correctamente"})
 		}
 
 		return res.status(500).json({code: 500, message : "Ocurrio un error"})
@@ -77,50 +56,47 @@ pokemon.patch("/:id([0-9]{1,3})", async (req,res,next) => {
 })
 
 admin.get("/", async (req,res,next) => {
-	const pkmn = await db.query("SELECT * FROM user");
-	res.status(200).json({code: 200, message : pkmn})
+	const usr = await db.query("SELECT * FROM user");
+	res.status(200).json({code: 200, message : usr})
 })
 
-pokemon.get("/:id([0-9]{1,3})", async (req, res, next) => {
+admin.get("/:id([0-9]{1,3})", async (req, res, next) => {
 	const id = req.params.id
-	const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
-	if (id >= 0 && id < 150) {
-		res.status(200).json({ code: 200, message: pkmn})
+	const usr = await db.query("SELECT * FROM user WHERE user_id=?;",[id]);
+	if (usr.length > 0) {
+		res.status(200).json({ code: 200, message: usr})
 	} else {
-		res.status(404).json({ code: 404, message: "Pokemon no encontrado"})
+		res.status(404).json({ code: 404, message: "Usuario no encontrado"})
 	}
 })
 
-pokemon.get("/:name([A-za-z]+)", async (req, res, next) => {
+admin.get("/:name([A-za-z]+)", async (req, res, next) => {
 	const name = req.params.name
-	const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='"+name+"';");
+	const usr = await db.query("SELECT * FROM user WHERE user_name=?;",[name]);
 
-	(pkmn.length > 0) ? 
-		res.status(200).json({ code: 200, message: pkmn}) : 
-		res.status(404).json({ code: 404, message: "Pokemon no encontrado"})
+	(usr.length > 0) ? 
+		res.status(200).json({ code: 200, message: usr}) : 
+		res.status(404).json({ code: 404, message: "Usuario no encontrado"})
 })
 
-admin.post("/login", async (req,res,next) => {
-	const {user_mail, user_password} = req.body
+admin.post("/signup", async (req,res,next) => {
+	const {user_name, user_mail, user_password, user_lastname, user_phone, user_address} = req.body
 
-	if (user_mail && user_password) {
+	if (user_name && user_mail && user_password, user_lastname, user_phone, user_address) {
 
-		const query = `SELECT * FROM user WHERE user_mail = ? AND user_password = ? AND role = 1;`
-		const rows = await db.query(query,[user_mail,user_password])
+		let query = `INSERT INTO user(user_name, user_lastname, user_mail, user_password, user_phone, user_address) ` 
+		query += ` VALUES (?, ?, ?, ?, ?, ?); `
 
-		if (rows.length == 1) {
-			const token = jwt.sign({
-				user_id: rows[0].user_id,
-				user_mail: rows[0].user_mail
-			}, "debugkey")
-			return res.status(200).json({code: 200, message : token})
+		const rows = await db.query(query, [user_name, user_lastname, user_mail, user_password, user_phone, user_address])
 
-		} else {
-			return res.status(200).json({code: 200, message : "Usuario y/o contraseña incorrectos"})
+		if (rows.affectedRows == 1) {
+			return res.status(201).json({code: 201, message:"Usuario agregado correctamente"})
 		}
-	}
-	return res.status(500).json({code:500,message:"Campos incompletos"}) 	
+		return res.status(500).json({code:500,message:"Ocurrio un error"})
 
+	}
+
+	return res.status(500).json({code:500,message:"Campos incompletos"})
 })
 
 module.exports = admin
--- routes/user.js
+++ routes/user.js
@@ -3,30 +3,27 @@ const user = express.Router();
 const db = require("../config/database")
 const jwt = require("jsonwebtoken")
 
-user.post("/signup", async (req,res,next) => {
-	const {user_name, user_mail, user_password} = req.body
+user.post("/login", async (req,res,next) => {
+	const {user_mail, user_password} = req.body
 
-	if (user_name && user_mail && user_password) {
+	if (user_mail && user_password) {
 
-		let query = `INSERT INTO user(user_name, user_mail, user_password) ` 
-		query += ` VALUES (?, ?, ?); `
+		const query = `SELECT * FROM user WHERE user_mail = ? AND user_password = ? AND role = 1;`
+		const rows = await db.query(query,[user_mail,user_password])
 
-		const rows = await db.query(query, [user_name,user_mail,user_password])
+		if (rows.length == 1) {
+			const token = jwt.sign({
+				user_id: rows[0].user_id,
+				user_mail: rows[0].user_mail
+			}, "debugkey")
+			return res.status(200).json({code: 200, message : token})
 
-		if (rows.affectedRows == 1) {
-			return res.status(201).json({code: 201, message:"Usuario agregado correctamente"})
+		} else {
+			return res.status(200).json({code: 200, message : "Usuario y/o contraseña incorrectos"})
 		}
-		return res.status(500).json({code:500,message:"Ocurrio un error"})
-
 	}
+	return res.status(500).json({code:500,message:"Campos incompletos"}) 	
 
-	return res.status(500).json({code:500,message:"Campos incompletos"})
 })
-
-
-// user.get("/", async (req,res,next) => {
-// 	const usr = await db.query("SELECT * FROM user");
-// 	res.status(200).json({code: 200, message : usr})
-// })
 
 module.exports = user
